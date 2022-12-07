765bfd0924c0bd0927184e7ac7b1249aadd69b61
Gustaluna <lgsantana.cin@gmail.com>
2022-12-07 00:58:49 -0300

    loop for

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-function tocaSom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play()
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
 
-let listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0
+/*let contador = 0
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom
+    
+    const tecla = listaDeTeclas [contador].classList[1]
+    const instumentro = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom(idAudio)
+    } 
 
     contador = contador + 1
 
     console.log(contador)
-} 
+} */
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas [contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (e) {
+        if(e.code === 'Space' || e.code === 'Enter'){
+        tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup= function () {
+        tecla.classList.remove('ativa')
+    }
+
+}
